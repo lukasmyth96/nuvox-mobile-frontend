bec0daca31a42751bf0d26a58d45167984bfeba2
lukasmyth96 <luka@nate.tech>
2021-03-05 16:40:18 +0000

    got initial grid layout

--- src/components/Keyboard/index.js
+++ src/components/Keyboard/index.js
@@ -1,25 +1,75 @@
-import React, { useRef } from "react";
+import React, {useRef} from "react";
 import useMouse from "@react-hook/mouse-position";
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
-import styles from './Keyboard.module.css';
+import styles from './Keyboard.module.css'
+
 
 const Keyboard = () => {
 
-  const target = useRef(null);
-  const mouse = useMouse(target, {
-    fps: 60,
-    enterDelay: 100,
-    leaveDelay: 100
-  });
+    const target = useRef(null);
+    const mouse = useMouse(target, {
+        fps: 60,
+        enterDelay: 100,
+        leaveDelay: 100
+    });
+
+    console.log(mouse.x / mouse.elementWidth, mouse.y / mouse.elementHeight)
 
     return (
-    <div>
-      <h2>useMouse()</h2>
-      <div className={styles.useMouseBox} ref={target}>
-        <div>{JSON.stringify(mouse, null, 2)}</div>
-      </div>
-    </div>
-    )
+            <Grid
+              container
+              direction="column"
+              justify="space-evenly"
+              alignItems="stretch"
+              spacing={0}>
+                <Grid item xs={12}>
+                    <Paper className={styles.paper}>xs=12</Paper>
+                </Grid>
+                <Grid container item spacing={0}>
+                    <Grid item xs={4}>
+                        <Paper className={styles.paper}>xs=6</Paper>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Paper className={styles.paper}>xs=6</Paper>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Paper className={styles.paper}>xs=6</Paper>
+                    </Grid>
+                </Grid>
+
+                <Grid container item spacing={0} ref={target}>
+                    <Grid item xs={4}>
+                        <Paper className={styles.paper}>xs=6</Paper>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Paper className={styles.paper}>xs=6</Paper>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Paper className={styles.paper}>xs=6</Paper>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Paper className={styles.paper}>xs=6</Paper>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Paper className={styles.paper}>xs=6</Paper>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Paper className={styles.paper}>xs=6</Paper>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Paper className={styles.paper}>xs=6</Paper>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Paper className={styles.paper}>xs=6</Paper>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Paper className={styles.paper}>xs=6</Paper>
+                    </Grid>
+                </Grid>
+            </Grid>
+    );
 }
 
 export default Keyboard;
