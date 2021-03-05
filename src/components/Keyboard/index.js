05981fce3b0e310a1374a2940c41472c3e847b89
lukasmyth96 <luka@nate.tech>
2021-03-05 17:44:57 +0000

    got mouse trail working

--- src/components/Keyboard/index.js
+++ src/components/Keyboard/index.js
@@ -9,13 +9,43 @@ import styles from './Keyboard.module.css'
 const Keyboard = () => {
 
     const target = useRef(null);
+    let drawPts = React.useRef([]);
     const mouse = useMouse(target, {
-        fps: 60,
-        enterDelay: 100,
-        leaveDelay: 100
+        fps: Infinity,
+        enterDelay: 0,
+        leaveDelay: 0
     });
 
-    console.log(mouse.x / mouse.elementWidth, mouse.y / mouse.elementHeight)
+    if (mouse.isDown) {
+        // An array is a terrible data structure for this :P
+        if (drawPts.current.length === 30) drawPts.current.shift();
+        drawPts.current.push(mouse);
+    } else{
+        drawPts.current = [];
+    }
+
+    const drawing = [];
+
+    for (let i = 0; i < drawPts.current.length; i++) {
+        const {pageX, pageY } = drawPts.current[i];
+        const transparency = 0.5 + 0.5 * (i / 30);
+
+        drawing.push(
+            <div
+                key={i}
+                style={{
+                    position: "absolute",
+                    left: pageX,
+                    top: pageY,
+                    width: 12,
+                    height: 12,
+                    backgroundColor: `rgba(255, 80, 80, ${transparency})`,
+                    borderRadius: "50%",
+                    boxShadow: `0px 0px 5px 5px rgba(255, 80, 80, ${transparency})`
+                }}
+            />
+        );
+    }
 
     return (
         <div className={styles.keyboardContainer}>
@@ -41,6 +71,7 @@ const Keyboard = () => {
                 </Grid>
 
                 <Grid container item spacing={1} ref={target}>
+                    {drawing}
                     <Grid item xs={4}>
                         <Paper elevation={3} className={styles.paper}>A B C</Paper>
                     </Grid>
