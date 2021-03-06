6e1418140d089a0bab299eddccf543302cf1630a
lukasmyth96 <luka@nate.tech>
2021-03-06 15:10:33 +0000

    comment in axiosInstance.js on how to switch to mobile testing

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8000/"
+    // replace with http://192.168.1.193:8000/ for mobile testing.
+    baseURL: "http://192.168.1.193:8000/"
 });
 
 export default axiosInstance;
